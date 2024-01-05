fc1c084e3c9320469a8e415e728b615fe086cc26
“Daniel-Notice” <danielnoticeworkv2@gmail.com>
2024-01-05 14:06:31 +0000

    only looking to see if i can make it work. will make it make sense later

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ const computerChoice = function(){
     let choices = ["Rock","Paper","Scissors"]
     let choice = choices[Math.ceil(Math.random()* choices.length - 1)]
     console.log(choice)
+    return choice
 }
-computerChoice()
+
+
+const round = function(playerselection = "Rock", computerSelection){
+    computerSelection = computerChoice()
+    
+    if (playerselection === computerSelection){
+        console.log("Its a tie")
+    }else if (playerselection === "Rock" && computerSelection === "Scissors" || playerselection === "Scissors" && computerSelection === "Paper" || playerselection === "Paper" && computerSelection === "Rock" ){
+        console.log("player wins")
+    }else{
+        console.log("computer wins")
+    }
+
+}
+
+round()
